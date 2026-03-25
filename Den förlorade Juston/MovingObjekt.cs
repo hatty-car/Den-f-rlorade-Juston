88af580f8623c931e8b2402adb6bb4b51f3f8192
Gustav Nilsson <gustav.nilsson7@learnet.se>
2026-03-25 12:42:48 +0100

    all arv

---
+++ Den förlorade Juston/MovingObjekt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Den_förlorade_Juston
+{
+    internal class MovingObjekt: Object
+    {
+    }
+}
