6cfebe5ea75f2c792453f1c03dc34baefa570259
Gustav Nilsson <gustav.nilsson7@learnet.se>
2026-04-27 14:21:01 +0200

    dswd

--- Den förlorade Juston/Game1.cs
+++ Den förlorade Juston/Game1.cs
@@ -51,8 +51,8 @@ namespace Den_förlorade_Juston
             bakgrundSkog = Content.Load<Texture2D>("forestBackground");
             bakgrund2 = Content.Load<Texture2D>("secondBackground");
 
-            Data.level1 = new StationaryLevelObjekt(Data.tileSet, 145, 24, 64, 7);
-            Data.level2 = new SpikeTilemap(Data.tileSet, 145, 25, 64, 7);
+            Data.level1 = new StationaryLevelObjekt(Data.tileSet, 145, 25, 64, 7);
+            Data.level2 = new SpikeTilemap(Data.tileSet, 145, 24, 64, 7);
             Data.level3 = new Leveldecoration(Data.tileSet, 145, 24, 64, 7);
             Data.All.Add(new Spelare(Data.spelarBild, new Vector2(0f,  0f), new Vector2(200, 1165)));
        
